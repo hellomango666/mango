3029cc98163e65b1266a8a2b862df39c5abb87a3
mango <test666@126.com>
2021-12-29 18:00:26 +0800

    333

---
+++ testA/src/testA/Hello.java
@@ -0,0 +1,10 @@
+package testA;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aa23232323323332323aabbbaaaaaaa");
+	}
+
+}
